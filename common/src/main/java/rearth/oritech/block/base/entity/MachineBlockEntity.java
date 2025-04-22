076d5af73b557dcc52e6125c08b41585055e4cab
fordarian <fordarian@gmail.com>
2025-04-22 07:52:56 +0700

    portal to save location

--- common/src/main/java/rearth/oritech/block/base/entity/MachineBlockEntity.java
+++ common/src/main/java/rearth/oritech/block/base/entity/MachineBlockEntity.java
@@ -437,7 +437,8 @@ public abstract class MachineBlockEntity extends BlockEntity
     public void cycleInputMode() {
         switch (inventoryInputMode) {
             case FILL_LEFT_TO_RIGHT:
-                inventoryInputMode = InventoryInputMode.FILL_EVENLY;
+                // TODO: enable after the fix )
+                // inventoryInputMode = InventoryInputMode.FILL_EVENLY;
                 break;
             case FILL_EVENLY:
                 inventoryInputMode = InventoryInputMode.FILL_LEFT_TO_RIGHT;
--- common/src/main/java/rearth/oritech/util/PortalEntity.java
+++ common/src/main/java/rearth/oritech/util/PortalEntity.java
@@ -5,8 +5,12 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import rearth.oritech.Oritech;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager;
@@ -21,6 +25,8 @@ public class PortalEntity extends Entity implements GeoEntity {
     private int age = 0;
     
     public Vec3d target;
+    public RegistryKey<World> targetWorld;
+    
     protected static final RawAnimation PORTAL = RawAnimation.begin().thenPlay("create").thenLoop("idle");
     
     
@@ -36,11 +42,53 @@ public class PortalEntity extends Entity implements GeoEntity {
     
     @Override
     public void onPlayerCollision(PlayerEntity player) {
-        if (target != null) {
-            player.teleport(target.x, target.y, target.z, true);
+        // Return early if on client side
+        if (this.getWorld().isClient) {
+            return;
         }
         
-        this.remove(RemovalReason.DISCARDED);
+        // Always remove the entity after processing the collision
+        defer(() -> this.remove(RemovalReason.DISCARDED));
+        
+        // Check if teleport information is missing
+        if (target == null || targetWorld == null) {
+            Oritech.LOGGER.info("Player {} has no teleport target or target dimension!", 
+                player.getName().getString());
+            return;
+        }
+        
+        Oritech.LOGGER.info("Player {} will go to {} in {}", 
+            player.getName().getString(), target, targetWorld.getValue());
+        
+        // Check if player is a server player
+        if (!(player instanceof ServerPlayerEntity serverPlayer)) {
+            Oritech.LOGGER.warn("Player {} is not a ServerPlayerEntity", 
+                player.getName().getString());
+            return;
+        }
+        
+        // Get destination world
+        ServerWorld destWorld = serverPlayer.getServer().getWorld(targetWorld);
+        if (destWorld == null) {
+            Oritech.LOGGER.warn("Target dimension {} not found!", 
+                targetWorld.getValue());
+            return;
+        }
+        
+        // Teleport the player
+        serverPlayer.teleport(
+            destWorld,
+            target.x, target.y, target.z,
+            serverPlayer.getYaw(), serverPlayer.getPitch()
+        );
+        
+        Oritech.LOGGER.info("Teleported {} to {} in {}", 
+            serverPlayer.getName().getString(), target, targetWorld.getValue());
+    }
+    
+    // Helper method to ensure cleanup happens
+    private void defer(Runnable action) {
+        action.run();
     }
     
     @Override
